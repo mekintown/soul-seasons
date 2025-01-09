277f08c8890a670db5ea8f8a29bfdc66d9c4b091
Maekin <74455990+mekintown@users.noreply.github.com>
2025-01-10 02:00:48 +0700

    Fix test

--- src/app/[locale]/example/page.test.tsx
+++ src/app/[locale]/example/page.test.tsx
@@ -6,10 +6,6 @@ describe("Page Component", () => {
   it("renders the component correctly with translations", () => {
     const page = render(<Page />);
 
-    expect(page.getByText("Hello")).toBeInTheDocument();
-
-    const linkElement = page.getByRole("link", { name: /button/i });
-    expect(linkElement).toHaveAttribute("href", "/1-2");
-    expect(linkElement).toBeInTheDocument();
+    expect(page.getByText("Hello world!")).toBeInTheDocument();
   });
 });
