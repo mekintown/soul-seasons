2373fe30fe4853959ce9ee6290be8d0fe991f3da
Maekin <74455990+mekintown@users.noreply.github.com>
2025-01-10 01:17:46 +0700

    setup translation

---
+++ src/app/[locale]/example/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { Link } from "@/i18n/routing";
+import { useTranslations } from "next-intl";
+import { motion } from "framer-motion";
+
+type Props = {};
+
+const Page = (props: Props) => {
+  const t = useTranslations("0");
+
+  return (
+    <div className="absolute h-full w-full bg-cover bg-no-repeat">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          duration: 1,
+          delay: 1,
+        }}
+        className="flex flex-col items-center text-center text-base text-woh-black"
+      >
+        <p className="mt-2 w-80 text-woh-black">{t("title")}</p>
+
+        <div className="absolute bottom-[10vh]">
+          <Link href="/1-2"></Link>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,30 @@
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
+
+const LocaleLayout = async ({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) => {
+  if (!routing.locales.includes(locale as any)) {
+    notFound();
+  }
+
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+};
+
+export default LocaleLayout;
