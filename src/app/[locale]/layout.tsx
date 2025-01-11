e8cd6d9e22d69d43de0aed93f1c96102790ed548
LittleNa1000 <naphanccnp@gmail.com>
2025-01-12 03:31:48 +0700

    chore: setup sound

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,33 +1,35 @@
-import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
-import { notFound } from "next/navigation";
-import { routing } from "@/i18n/routing";
-import "../globals.css";
+import { NextIntlClientProvider } from "next-intl"
+import { getMessages } from "next-intl/server"
+import { notFound } from "next/navigation"
+import { routing } from "@/i18n/routing"
+import "../globals.css"
+import Sounds from "@/components/sounds"
 
 const LocaleLayout = async ({
   children,
   params,
 }: {
-  children: React.ReactNode;
-  params: Promise<{ locale: string }>;
+  children: React.ReactNode
+  params: Promise<{ locale: string }>
 }) => {
-  const { locale } = await params;
+  const { locale } = await params
 
   if (!routing.locales.includes(locale as any)) {
-    notFound();
+    notFound()
   }
 
-  const messages = await getMessages();
+  const messages = await getMessages()
 
   return (
     <html lang={locale}>
       <body>
         <NextIntlClientProvider messages={messages}>
+          <Sounds />
           {children}
         </NextIntlClientProvider>
       </body>
     </html>
-  );
-};
+  )
+}
 
-export default LocaleLayout;
+export default LocaleLayout
