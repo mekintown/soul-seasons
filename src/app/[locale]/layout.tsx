21e0674ac949d6e01cf82b9481d724167ff92673
Naphan Choatchuangnapha <83999105+LittleNa1000@users.noreply.github.com>
2025-01-14 11:16:06 +0700

    Feat: sound setup and style root layout (#2)

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,33 +1,39 @@
-import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
-import { notFound } from "next/navigation";
-import { routing } from "@/i18n/routing";
-import "../globals.css";
+import { NextIntlClientProvider } from "next-intl"
+import { getMessages } from "next-intl/server"
+import { notFound } from "next/navigation"
+import { routing } from "@/i18n/routing"
+import "../globals.css"
+import Sounds from "@/components/sounds"
+import SoundToggle from "@/components/sound-toggle"
 
 const LocaleLayout = async ({
   children,
   params,
 }: {
-  children: React.ReactNode;
-  params: Promise<{ locale: string }>;
+  children: React.ReactNode
+  params: Promise<{ locale: string }>
 }) => {
-  const { locale } = await params;
+  const { locale } = await params
 
   if (!routing.locales.includes(locale as any)) {
-    notFound();
+    notFound()
   }
 
-  const messages = await getMessages();
+  const messages = await getMessages()
 
   return (
     <html lang={locale}>
-      <body>
+      <body className="relative mx-auto min-h-screen w-full max-w-[393px] overflow-x-hidden overscroll-none">
         <NextIntlClientProvider messages={messages}>
+          <Sounds />
+          <div className="absolute right-8 top-12 z-50 flex w-full items-center justify-end">
+            <SoundToggle />
+          </div>
           {children}
         </NextIntlClientProvider>
       </body>
     </html>
-  );
-};
+  )
+}
 
-export default LocaleLayout;
+export default LocaleLayout
