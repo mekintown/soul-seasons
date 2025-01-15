598e1f0de71bfbe1a250671999d3b06bc1dfcd24
Tt <tt.taiton@gmail.com>
2025-01-16 03:35:21 +0700

    add size and boxpadding props into messagebox

--- src/components/ui/messagebox.tsx
+++ src/components/ui/messagebox.tsx
@@ -1,12 +1,19 @@
-const MessageBox = ({ message }: { message: string })=>{
-    return(
-    <div className=" flex justify-center items-center h-full">
-        <div className="w-[265px] h-[207px] opacity-[70%] bg-white flex items-center justify-center rounded-[12px] ">
-            <p className="w-[227px] h-[175px] text-center">{message}</p>
-        </div>
-        
-    </div>
-    )
+import React from "react";
+
+interface MessageBoxProps {
+  message: React.ReactNode; // Accepts JSX, including bold text
+  size?: string; // Optional className for customization
+  BoxPadding? : string;
 }
+
+const MessageBox: React.FC<MessageBoxProps> = ({ message, size, BoxPadding }) => {
+  return (
+    <div className={`flex justify-center items-center h-full ${size || ""}`}>
+      <div className={`w-full h-full opacity-70 bg-white flex items-center justify-center rounded-lg ${BoxPadding || ''}`}>
+        <div className="text-center relative">{message}</div>
+      </div>
+    </div>
+  );
+};
 
 export default MessageBox;
