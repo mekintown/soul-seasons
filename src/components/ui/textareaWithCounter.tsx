60e64b455cb2d949ce486fcd00540198bfa88670
LittleNa1000 <naphanccnp@gmail.com>
2025-01-16 17:14:21 +0700

    style: textareaWithCounter

--- src/components/ui/textareaWithCounter.tsx
+++ src/components/ui/textareaWithCounter.tsx
@@ -1,19 +1,22 @@
 import { ChangeEvent } from "react"
 import { Textarea } from "./textarea"
+import { cn } from "@/lib/utils"
 
 export const TextareaWithCounter = ({
   onChange,
   value,
   maxCount,
+  className,
 }: {
   onChange: (e: ChangeEvent<HTMLTextAreaElement>) => void
   value: string
   maxCount?: number
+  className?: string
 }) => {
   return (
-    <div className="relative">
+    <div className={cn(className, "relative")}>
       <Textarea
-        className=" bg-white border-none rounded-xl"
+        className=" bg-white border-none rounded-xl h-full resize-none text-lg font-light py-2 px-4"
         onChange={(e) => {
           if (!maxCount || e.target.value.length <= maxCount) {
             onChange(e)
