805934e681fcf897e05f7e6ca8676d40f7aea63e
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2025-01-16 22:34:28 +0700

    add scene

---
+++ src/components/interactive-background.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { backgroundMapConfig } from "@/lib/bg-config";
+import { usePathname, useRouter } from "@/i18n/routing";
+import { useEffect, useMemo, useState } from "react";
+import AnimatedImage from "./animated-image";
+
+const InteractiveBackground = () => {
+  const path = usePathname();
+  const router = useRouter();
+  const page = path.split("/")[1] as keyof typeof backgroundMapConfig;
+  const [bgImgSrc, setBgImgSrc] = useState<string>();
+
+  useEffect(() => {
+    switch (page) {
+      case "2-7":
+        backgroundMapConfig[page].image.forEach((image, index) => {
+          setTimeout(() => {
+            setBgImgSrc(image);
+            if (index === backgroundMapConfig[page].image.length - 1) {
+              setTimeout(() => {
+                router.push("2-8");
+              }, backgroundMapConfig[page].stopMotionDuration / 2);
+            }
+          }, index * backgroundMapConfig[page].stopMotionDuration);
+        });
+        break;
+      case "2-9":
+        backgroundMapConfig[page].image.forEach((image, index) => {
+          setTimeout(() => {
+            setBgImgSrc(image);
+            if (index === backgroundMapConfig[page].image.length - 1) {
+              setTimeout(() => {
+                router.push("2-10");
+              }, backgroundMapConfig[page].stopMotionDuration / 2);
+            }
+          }, index * backgroundMapConfig[page].stopMotionDuration);
+        });
+        break;
+
+      default:
+        setBgImgSrc(backgroundMapConfig[page].image);
+        break;
+    }
+  }, [page, router]);
+
+  const imagePreloadSrc = useMemo(() => {
+    const newImagePreloadSrc = backgroundMapConfig[page].imagePreload;
+    return newImagePreloadSrc;
+  }, [page]);
+  return (
+    <>
+      {bgImgSrc && (
+        <AnimatedImage
+          src={bgImgSrc}
+          preloadSrcs={imagePreloadSrc}
+          alt="background-image"
+          loading="eager"
+          fill
+          className="relative -z-50 object-cover"
+        />
+      )}
+    </>
+  );
+};
+
+export default InteractiveBackground;
---
+++ src/lib/bg-config.ts
@@ -0,0 +1,86 @@
+export const backgroundMapConfig = {
+  "1-2-1": {
+    image: "/img/1-2.webp",
+    imagePreload: [],
+  },
+  "1-2-2": {
+    image: "/img/1-2.webp",
+    imagePreload: [],
+  },
+  "1-2-3": {
+    image: "/img/1-2.webp",
+    imagePreload: [],
+  },
+  "1-2-4": {
+    image: "/img/1-2.webp",
+    imagePreload: [],
+  },
+  "1-2-5": {
+    image: "/img/1-2.webp",
+    imagePreload: [],
+  },
+  "1-2-6": {
+    image: "/img/1-2.webp",
+    imagePreload: ["/img/1-3-1.png"],
+  },
+  "1-3-1": {
+    image: "/img/1-3-1.png",
+    imagePreload: ["/img/1-3-2.png"],
+  },
+  "1-3-2": {
+    image: "/img/1-3-2.png",
+    imagePreload: ["/img/1-3-3.png"],
+  },
+  "1-3-3": {
+    image: "/img/1-3-3.png",
+    imagePreload: ["/img/1-3-4.webp"],
+  },
+  "1-3-4": {
+    image: "/img/1-3-4.png",
+    imagePreload: ["/img/1-3-5.webp"],
+  },
+  "1-3-5": {
+    image: "/img/1-3-5.png",
+    imagePreload: ["/img/1-3-6.png"],
+  },
+  "1-3-6": {
+    image: "/img/1-3-6.png",
+    imagePreload: ["/img/1-4-1.png"],
+  },
+  "1-4": {
+    image: "/img/1-4.webp",
+    imagePreload: ["/img/1-5.webp"],
+  },
+  "2-7": {
+    image: ["/img/2-1.webp", "/img/2-2.webp", "/img/2-3.webp"],
+    stopMotionDuration: 1200,
+    imagePreload: [],
+  },
+  "2-8": {
+    image: "/img/2-6.webp",
+    imagePreload: [
+      "/img/2-9-1.webp",
+      "/img/2-9-2.webp",
+      "/img/2-9-3.webp",
+      "/img/2-9-4.webp",
+      "/img/2-9-5.webp",
+      "/img/2-9-6.webp",
+      "/img/2-9-7.webp",
+      "/img/2-9-8.webp",
+    ],
+  },
+  "2-9": {
+    image: [
+      "/img/2-9-1.webp",
+      "/img/2-9-2.webp",
+      "/img/2-9-3.webp",
+      "/img/2-9-4.webp",
+      "/img/2-9-5.webp",
+      "/img/2-9-6.webp",
+      "/img/2-9-7.webp",
+      "/img/2-9-8.webp",
+    ],
+    stopMotionDuration: 1200,
+    imagePreload: [],
+  },
+} as const;
