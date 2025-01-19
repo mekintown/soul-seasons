10a646f66fd9c8be15845dd8b02eb5495a8148dc
Tt <tt.taiton@gmail.com>
2025-01-19 13:34:18 +0700

    Remove Trans

--- src/components/scene/scene0-2.tsx
+++ src/components/scene/scene0-2.tsx
@@ -1,10 +1,9 @@
 "use client";
-
 import MessageBox from "../ui/messagebox";
 import Shadow from "../ui/Shadow";
 import Background1 from "../../img/background/0.png";
 import { useRouter } from "next/navigation";
-import { Trans } from 'react-i18next';
+
 
 const Scene2 = () => {
   const router = useRouter();
@@ -22,12 +21,12 @@ const Scene2 = () => {
       }}
     >
       <Shadow>
-        {/* Warning Header */}
+        
         <div className="flex justify-end items-center h-full flex-col">
           <h1 className="font-kanit text-primaryblue text-[28px] mb-[10px]">คำเตือน</h1>
         </div>
 
-        {/* Message Box */}
+       
         <div className="flex justify-end items-center h-full flex-col">
           <MessageBox
             size="w-[274px] h-auto"
@@ -35,29 +34,28 @@ const Scene2 = () => {
             message={
               
               <div className="flex flex-col text-primaryblue font-kanit font-light text-[18px] w-[243px] leading-6 space-y-3">
-               <Trans i18nKey="Zero2.p1">
+               
                 <p>
                   เว็บไซต์นี้นำเสนอเนื้อหาเกี่ยวกับ
                   <span className="font-semibold"> การค้นหาตัวตนที่สูญหายและ</span>
                   <span className="font-semibold"> การทำความเข้าใจกับความกังวลและ</span>
                   <span className="font-semibold"> ความกลัวที่อยู่ในจิตใจของคุณ</span>
                 </p>
-                </Trans>
-                <Trans i18nKey="Zero2.p2">
-                  <p>
+             
+                <p>
                     เนื้อหาภายในเรื่องเกี่ยวกับ
                     <span className="font-semibold">
                       เป้าหมายที่ยังไปไม่ถึง การเผชิญอุปสรรคและความล้มเหลว
                     </span>
                     ไม่ควรนำไปคิดต่อทั้งตัวเนื้อเรื่อง และชีวิตจริง
                   </p>
-                </Trans>
-                <Trans i18nKey="Zero2.p3">
+                
+                
                   <p>
                     หากคุณรู้สึกไม่สบายใจระหว่างการเล่น คุณสามารถหยุดได้ทันที
                     <span className="font-semibold"> เพราะความรู้สึกของคุณสำคัญที่สุด</span>
                   </p>
-                </Trans>
+                
                
             
               </div>
