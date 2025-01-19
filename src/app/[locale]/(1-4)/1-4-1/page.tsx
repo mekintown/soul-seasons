4bde5c912c129cc5901f18888e41f302b06ad5cb
Naphan Choatchuangnapha <83999105+LittleNa1000@users.noreply.github.com>
2025-01-19 22:44:08 +0700

    feat: 1-4-1, 1-4-2, textareaWithCounter, and 1-4-6 for demo textareaWithCounter (#7)
    * feat: draft 1-4-1
    
    * feat: draft textareaWithCounter
    
    * style: textareaWithCounter
    
    * feat(1-4-1): get name from localStorage and add translation
    
    * feat(1-4-2): page and 1-4-6 for demo textareaWithCounter
    
    * fix(1-4-6): build
    
    * feat: 1-4-2 next button
    
    * Fix file structure
    
    ---------
    
    Co-authored-by: Maekin <74455990+mekintown@users.noreply.github.com>

---
+++ src/app/[locale]/(1-4)/1-4-1/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { motion } from "framer-motion";
+import { useTranslations } from "next-intl";
+import React from "react";
+
+const Scene1_4Page1: React.FC = () => {
+  const t = useTranslations("1-4-1");
+  const name =
+    typeof window !== "undefined" ? localStorage.getItem("name") : null;
+  const importantPerson =
+    typeof window !== "undefined"
+      ? localStorage.getItem("importantPerson")
+      : null;
+
+  return (
+    <div className="flex h-screen w-full bg-[url('/bg/1-4-1.webp')] flex-col items-center bg-cover bg-no-repeat bg-center">
+      <div className="flex flex-col items-center justify-center">
+        <motion.div
+          className="absolute bottom-[62%]"
+          initial={{ opacity: 0, y: 0 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.5, duration: 1 }}
+        >
+          <div className="flex h-[135px] w-[400px]">
+            <div className="items-cemter flex h-full w-full flex-col justify-center">
+              <p className="text-center text-lg text-white font-light whitespace-break-spaces">
+                {t("thereMightBe")}
+              </p>
+            </div>
+          </div>
+        </motion.div>
+        <motion.div
+          className="absolute bottom-[51%] right-[24%] w-36 h-10 flex items-center justify-center text-center text-sm text-black"
+          initial={{ opacity: 0, y: 0 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.5, duration: 1 }}
+        >
+          {name ?? t("name")} & {importantPerson ?? t("importantPerson")}
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default Scene1_4Page1;
---
+++ src/components/ui/textareaWithCounter.tsx
@@ -0,0 +1,34 @@
+import { ChangeEvent } from "react"
+import { Textarea } from "./textarea"
+import { cn } from "@/lib/utils"
+
+export const TextareaWithCounter = ({
+  onChange,
+  value,
+  maxCount,
+  className,
+}: {
+  onChange: (e: ChangeEvent<HTMLTextAreaElement>) => void
+  value: string
+  maxCount?: number
+  className?: string
+}) => {
+  return (
+    <div className={cn(className, "relative")}>
+      <Textarea
+        className=" bg-white border-none rounded-xl h-full resize-none text-lg font-light py-2 px-4"
+        onChange={(e) => {
+          if (!maxCount || e.target.value.length <= maxCount) {
+            onChange(e)
+          }
+        }}
+        value={value}
+      />
+      {maxCount ? (
+        <div className="absolute bottom-1 right-3 text-[#8E8E93] text-lg font-light">
+          {value.length}/{maxCount}
+        </div>
+      ) : null}
+    </div>
+  )
+}
