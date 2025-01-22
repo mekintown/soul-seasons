d8f05331243f8750807fc500752f44b8a0fb1f5d
Zeeneemee <106436879+Zeeneemee@users.noreply.github.com>
2025-01-23 03:30:06 +0700

    Tt soul (#3)
    * scene 0-0-1
    
    * scene 0-2
    
    add routing with handleclick
    
    * mobile display body
    
    * add font and dissolve annimation
    
    * add size and boxpadding props into messagebox
    
    * create shadow component
    
    * edit scene 0,0-1,0-2,0-4
    
    * add font kanit
    
    * scene 0-3-1,0-3-2
    
    * scene 0-4-1,0-4-3 & name input storing in local storage
    
    * edit routing and tailwind animation data type
    
    * Deleted comment add primary color and remove font cloud
    
    * Remove Trans
    
    * add scene 2, 2-1 and refactor with update i18n
    
    * edit the error in th.json  and en.json format
    
    * edit back eng.json
    
    * edit margin for 0-3-1
    
    * adjust  styling margins
    
    * remove unused file
    
    * Refactor
    
    * Refactor
    
    * Add tech debt
    
    * Add disabled btn
    
    ---------
    
    Co-authored-by: Maekin <74455990+mekintown@users.noreply.github.com>

---
+++ src/components/BaseLayout.tsx
@@ -0,0 +1,27 @@
+import { pick } from "lodash";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages, setRequestLocale } from "next-intl/server";
+import { PropsWithChildren } from "react";
+
+interface BaseLayoutProps extends PropsWithChildren {
+  locale: string;
+  messageScope: string;
+}
+
+const BaseLayout = async ({
+  children,
+  locale,
+  messageScope,
+}: BaseLayoutProps) => {
+  setRequestLocale(locale);
+
+  const messages = await getMessages();
+
+  return (
+    <NextIntlClientProvider messages={pick(messages, [messageScope, "base"])}>
+      {children}
+    </NextIntlClientProvider>
+  );
+};
+
+export default BaseLayout;
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,11 +1,11 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 disabled:bg-gray-300 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
@@ -19,11 +19,13 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        next: "bg-primaryblue  rounded-lg text-white font-cloud font-bold animate-dissolve duration-300 ease-out animate",
       },
       size: {
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
+        xl: "h-12 rounded-md px-16",
         icon: "h-9 w-9",
       },
     },
@@ -32,26 +34,26 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = "Button";
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
