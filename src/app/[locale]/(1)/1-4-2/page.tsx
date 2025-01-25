f433b186e2f5b47582804006d0b334c0b4922908
Naphan Choatchuangnapha <83999105+LittleNa1000@users.noreply.github.com>
2025-01-25 17:03:03 +0700

    Feat: 1-4-1 to 1-4-9 scene (#9)

--- src/app/[locale]/(1)/1-4-2/page.tsx
+++ src/app/[locale]/(1)/1-4-2/page.tsx
@@ -1,46 +1,41 @@
-"use client";
-import { Link } from "@/i18n/routing";
-import { getLocalStorageWithFallback } from "@/lib/localstorageUtils";
-import { motion } from "framer-motion";
-import { useTranslations } from "next-intl";
-import React from "react";
+"use client"
+import DelayedFullScreenLink from "@/components/ui/DelayedFullScreenLink"
+import { Link } from "@/i18n/routing"
+import { getLocalStorageWithFallback } from "@/lib/localstorageUtils"
+import { motion } from "framer-motion"
+import { useTranslations } from "next-intl"
+import React from "react"
 
 const Scene1_4Page2: React.FC = () => {
-  const t = useTranslations("1-4-2");
+  const t = useTranslations("1-4-2")
 
-  const name = getLocalStorageWithFallback("name", t("name"));
-  const importantPerson = getLocalStorageWithFallback(
-    "importantPerson",
-    t("importantPerson")
-  );
+  const name = getLocalStorageWithFallback("name", t("name"))
+  const importantPerson = getLocalStorageWithFallback("importantPerson", t("importantPerson"))
 
   return (
-    <div className="flex h-screen w-full bg-[url('/bg/1-4-2.webp')] flex-col items-center bg-cover bg-no-repeat bg-center">
-      <div className="flex flex-col items-center justify-center">
-        <motion.div
-          className="absolute bottom-[49%] right-[19%] w-60 h-10 flex items-center justify-center text-center text-lg text-black"
-          initial={{ opacity: 0, y: 0 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.5, duration: 1 }}
-        >
-          {name ?? t("name")} &<br /> {importantPerson ?? t("importantPerson")}
-        </motion.div>
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <motion.div
+        className="absolute bottom-[49%] right-[19%] w-60 h-10 flex items-center justify-center text-center text-lg text-black"
+        initial={{ opacity: 0, y: 0 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.5, duration: 1 }}>
+        {name ?? t("name")} &<br /> {importantPerson ?? t("importantPerson")}
+      </motion.div>
 
-        <motion.div
-          className="absolute bottom-[39%] right-[4%] w-16 h-16"
-          initial={{ opacity: 0, y: 0 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 1, y: 0 }}
-          transition={{ delay: 1.5, duration: 1 }}
-        >
-          <Link href="/1-4-4">
-            <div className="w-full h-full rounded-full opacity-50 bg-[#D9D9D9]" />
-          </Link>
-        </motion.div>
-      </div>
+      <motion.div
+        className="absolute bottom-[39%] right-[4%] w-16 h-16"
+        initial={{ opacity: 0, y: 0 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 1, y: 0 }}
+        transition={{ delay: 1.5, duration: 1 }}>
+        <Link href="/1-4-4">
+          <div className="w-full h-full rounded-full opacity-50 bg-[#D9D9D9]" />
+        </Link>
+      </motion.div>
+      <DelayedFullScreenLink href="1-4-4" delay={2000} />
     </div>
-  );
-};
+  )
+}
 
-export default Scene1_4Page2;
+export default Scene1_4Page2
--- src/app/[locale]/(1)/1-4-4/page.tsx
+++ src/app/[locale]/(1)/1-4-4/page.tsx
@@ -1,24 +1,14 @@
 "use client"
-import NextButton from "@/components/ui/NextButton"
-import { TextareaWithCounter } from "@/components/ui/textareaWithCounter"
+import DelayedFullScreenLink from "@/components/ui/DelayedFullScreenLink"
 import { motion } from "framer-motion"
 import { useTranslations } from "next-intl"
 import React from "react"
 
-const Scene1_4Page6: React.FC = () => {
-  const t = useTranslations("1-4-6")
-  const [text, setText] = React.useState("")
+const Scene1_4Page4: React.FC = () => {
+  const t = useTranslations("1-4-4")
 
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center">
-      <motion.div
-        className="absolute bottom-[46%] right-[16%] flex items-center justify-center text-[64px] font-normal blur-sm"
-        initial={{ opacity: 0, y: 0 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.5, duration: 1 }}>
-        Goals!
-      </motion.div>
       <motion.div
         className="absolute bottom-[60%]"
         initial={{ opacity: 0, y: 0 }}
@@ -28,36 +18,30 @@ const Scene1_4Page6: React.FC = () => {
         <div className="flex h-[135px] w-full">
           <div className="items-cemter flex h-full w-full flex-col justify-center">
             <p className="text-center text-lg text-white font-light whitespace-break-spaces drop-shadow">
-              {t("whatIsAGoal")}
+              {t("theDiary")}
             </p>
           </div>
         </div>
       </motion.div>
       <motion.div
-        className="absolute bottom-[25%] w-full"
+        className="absolute bottom-[48%] right-[41.5%] flex items-center justify-center text-4xl font-medium"
         initial={{ opacity: 0, y: 0 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.5, duration: 1 }}>
-        <TextareaWithCounter
-          onChange={(e) => setText(e.target.value)}
-          value={text}
-          maxCount={80}
-          className="mx-12 h-[214px]"
-        />
+        Day 0:
       </motion.div>
       <motion.div
-        className="absolute bottom-[9%]"
+        className="absolute top-[53%] right-[11%] w-56 flex items-center justify-center text-lg text-black whitespace-break-spaces"
         initial={{ opacity: 0, y: 0 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.5, duration: 1 }}>
-        <div onClick={() => localStorage.setItem("goal", text)}>
-          <NextButton url="1-4-8" disabled={text.length === 0} />
-        </div>
+        {t("planting")}
       </motion.div>
+      <DelayedFullScreenLink href="1-4-5" delay={2000} />
     </div>
   )
 }
 
-export default Scene1_4Page6
+export default Scene1_4Page4
