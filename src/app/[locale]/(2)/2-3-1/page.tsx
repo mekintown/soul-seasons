e351b5afbd5eef8acbd722ed37d58fac4b3a03f1
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2025-01-30 20:17:07 +0700

    feat: add colored

--- src/app/[locale]/(2)/2-3-1/page.tsx
+++ src/app/[locale]/(2)/2-3-1/page.tsx
@@ -1,11 +1,20 @@
 "use client";
-import DelayedFullScreenLink from "@/components/ui/DelayedFullScreenLink";
-import NextDisplay from "@/components/ui/nextDisplay";
 import { getLocalStorageWithFallback } from "@/lib/localstorageUtils";
-import { motion } from "framer-motion";
 import { useTranslations } from "next-intl";
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+import DelayedFullScreenLink from "@/components/ui/DelayedFullScreenLink";
 
-const Page2_3_1 = () => {
+interface MotivationGoal {
+  index: string;
+  name: string;
+}
+
+const Scene2_3Page1: React.FC = () => {
+  const motivationGoals: MotivationGoal[] = JSON.parse(
+    getLocalStorageWithFallback("motivationGoal", "[]")
+  );
   const t = useTranslations("2-3-1");
 
   const importantPerson = getLocalStorageWithFallback(
@@ -17,8 +26,24 @@ const Page2_3_1 = () => {
     t("motivations")
   );
 
+  // TODO: Extract this into a util
+  const colorMapping: Record<string, string> = {
+    Family: "blue",
+    "Finance/Money": "yellow",
+    "Spirituality/Religion": "purple",
+    Health: "green",
+    "Relationships/Friends": "red",
+    "Sharing/Contribution": "orange",
+    "Career/Work": "pink",
+    "Self-Development": "cyan",
+  };
+
+  const selectedColors = motivationGoals
+    .map((goal) => colorMapping[goal.name] || "blue")
+    .slice(0, 3);
+
   return (
-    <div className="flex h-screen flex-col items-center justify-center relative">
+    <div className="flex h-screen w-full flex-col items-center justify-center relative">
       <motion.div
         className="text-center text-base text-white absolute top-1/4"
         initial={{ opacity: 0, z: -20 }}
@@ -35,14 +60,37 @@ const Page2_3_1 = () => {
         initial={{ opacity: 0 }}
         animate={{
           opacity: 1,
-          z: 10,
-          transition: { duration: 1, delay: 4 },
+          z: 0,
+          transition: { duration: 1, delay: 2 },
         }}
         exit={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.5, duration: 1 }}
-        className="flex justify-center absolute top-[85%] inset-x-0"
+        className="flex justify-center top-[85%] inset-x-0"
       >
-        <NextDisplay />
+        {/* Sparkles */}
+        {selectedColors.map((color) => (
+          <div
+            className="w-[130%] h-[130%] absolute -bottom-[200px]"
+            key={color}
+          >
+            <Image
+              src={`/img/motivation/sparkle-${color}-1x.webp`}
+              fill
+              objectFit="contain"
+              alt={`sparkle-${color}`}
+            />
+          </div>
+        ))}
+
+        {/* Christmas tree */}
+        <div className="w-[130%] h-[130%] absolute -bottom-[100px]">
+          <Image
+            src="/img/motivation/christmas-tree.webp"
+            fill
+            objectFit="contain"
+            alt="christmas-tree"
+          />
+        </div>
       </motion.div>
 
       <DelayedFullScreenLink href="2-3-2" delay={2000} />
@@ -50,4 +98,4 @@ const Page2_3_1 = () => {
   );
 };
 
-export default Page2_3_1;
+export default Scene2_3Page1;
