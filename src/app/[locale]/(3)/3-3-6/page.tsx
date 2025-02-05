24d03ac70f46e43e7ea440086dd245c79667452d
Naphan Choatchuangnapha <83999105+LittleNa1000@users.noreply.github.com>
2025-02-05 22:59:31 +0700

    feat: 3-3 scene (#15)

---
+++ src/app/[locale]/(3)/3-3-6/page.tsx
@@ -0,0 +1,23 @@
+"use client"
+import { Link } from "@/i18n/routing"
+import { motion } from "framer-motion"
+import React from "react"
+
+const Scene3_3Page6: React.FC = () => {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <motion.div
+        className="absolute bottom-[15%]"
+        initial={{ opacity: 0, y: 0 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0, duration: 1 }}>
+        <Link href="3-3-7">
+          <div className="bg-gradient-to-b from-[#D9D9D9] to-[#737373] w-[167px] h-[167px] rounded-full opacity-50 bg-blend-luminosity shadow-touch" />
+        </Link>
+      </motion.div>
+    </div>
+  )
+}
+
+export default Scene3_3Page6
