78cf044cc104ba7c28d2d0fbfbdba6f840b363b9
LittleNa1000 <naphanccnp@gmail.com>
2025-02-11 01:27:54 +0700

    feat: bar and marker styling

--- src/components/ui/bar.tsx
+++ src/components/ui/bar.tsx
@@ -1,59 +1,55 @@
-'use client'
-import { useEffect } from 'react';
-import High from '../../img/icons/high.png';
-import Low from '../../img/icons/low.png';
+"use client"
+import { useEffect } from "react"
+import Marker from "./marker"
 
 interface BarProps {
-  stressScore: number;
-  setStressScore: (score: number) => void;
-  setMouseDown: (mouseDown: boolean) => void;
+  stressScore: number
+  setStressScore: (score: number) => void
+  setMouseDown: (mouseDown: boolean) => void
 }
 
 const Bar: React.FC<BarProps> = ({ stressScore, setStressScore, setMouseDown }) => {
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setStressScore(Number(e.target.value));
-    setMouseDown(true);
-  };
+    setStressScore(Number(e.target.value))
+    setMouseDown(true)
+  }
 
   useEffect(() => {
-    localStorage.setItem('stressScore', stressScore.toString());
-  }, [stressScore]);
+    localStorage.setItem("stressScore", stressScore.toString())
+  }, [stressScore])
 
   return (
-    <div className="flex">
-      <div className="flex flex-col items-center justify-center  font-bold text-white">
-        <img src={Low.src} alt="High" />
-        1
-      </div>
-      <div className="flex flex-col items-center justify-center relative">
+    <div className="flex flex-col gap-1">
+      <div className="flex flex-col items-center justify-center relative ">
         {/* Bubble */}
         <div
-          className="absolute -top-6 bg-gray-300 text-black text-sm rounded-full px-2 py-1"
+          className="absolute -top-12 rounded-full"
           style={{
             left: `calc(${stressScore * 10}% - 20px)`, // Adjust position dynamically
-          }}
-        >
-          {stressScore}
+          }}>
+          <Marker stressScore={stressScore} />
         </div>
-
-       
         <input
           type="range"
           min="1"
           max="10"
           name="score"
-          className="w-[200px] h-[20px]"
+          className="w-[265px] h-5 drop-shadow-[4px_4px_4px_rgba(0,0,0,0.25)] custom-range rounded-xl"
           value={stressScore}
+          style={{
+            background: `linear-gradient(to right, #182649 ${stressScore * 10}%, #ffffff ${
+              stressScore * 10
+            }%)`,
+          }}
           onChange={handleInputChange}
         />
       </div>
-      <div className="flex flex-col items-center font-bold text-white">
-        <img src={High.src} alt="Low" />
-        10
+      <div className="flex justify-between text-lg font-light text-white">
+        <span>1</span>
+        <span>10</span>
       </div>
     </div>
-    
-  );
-};
+  )
+}
 
-export default Bar;
+export default Bar
