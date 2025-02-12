15a049d9c68e048850905c94924efc28b274eaaf
Maekin <74455990+mekintown@users.noreply.github.com>
2025-02-12 19:19:51 +0700

    Fix image path

--- src/lib/pots-config.ts
+++ src/lib/pots-config.ts
@@ -1,35 +1,53 @@
-// config/potConfig.ts
-import potOrange from '../img/icons/pot/potOrange.png';
-import potPurple from '../img/icons/pot/potPurple.png';
-import potGreen from '../img/icons/pot/potGreen.png';
-import potRed from '../img/icons/pot/potRed.png';
-import potPink from '../img/icons/pot/potPink.png';
-import potYellow from '../img/icons/pot/potYellow.png';
-import potBlue from '../img/icons/pot/potBlue.png';
-import potIndigo from '../img/icons/pot/potIndigo.png';
-
-// Glow pots
-import potIndigoGlow from '../img/icons/potGlow/potIndigoGlow.png';
-import potPurpleGlow from '../img/icons/potGlow/potPurpleGlow.png';
-import potOrangeGlow from '../img/icons/potGlow/potOrangeGlow.png';
-import potRedGlow from '../img/icons/potGlow/potRedGlow.png';
-import potGreenGlow from '../img/icons/potGlow/potGreenGlow.png';
-import potYellowGlow from '../img/icons/potGlow/potYellowGlow.png';
-import potBlueGlow from '../img/icons/potGlow/potBlueGlow.png';
-import potPinkGlow from '../img/icons/potGlow/potPinkGlow.png';
-
-
-// Define pot configuration
 export const potImagesLeft = [
-    { src: potPink.src, label: "Career/Work", pos: "", glow: potPinkGlow.src },
-    { src: potIndigo.src, label: "Family", pos: " right-[50px]", glow: potIndigoGlow.src },
-    { src: potRed.src, label: "Relationship/Friends", pos: "right-[50px]", glow: potRedGlow.src },
-    { src: potOrange.src, label: "Sharing/Contributions", pos: "", glow: potOrangeGlow.src },
+  {
+    src: "/icons/pot/potPink.png",
+    label: "Career/Work",
+    pos: "",
+    glow: "/icons/potGlow/potPinkGlow.png",
+  },
+  {
+    src: "/icons/pot/potIndigo.png",
+    label: "Family",
+    pos: " right-[50px]",
+    glow: "/icons/potGlow/potIndigoGlow.png",
+  },
+  {
+    src: "/icons/pot/potRed.png",
+    label: "Relationship/Friends",
+    pos: "right-[50px]",
+    glow: "/icons/potGlow/potRedGlow.png",
+  },
+  {
+    src: "/icons/pot/potOrange.png",
+    label: "Sharing/Contributions",
+    pos: "",
+    glow: "/icons/potGlow/potOrangeGlow.png",
+  },
 ];
 
 export const potImagesRight = [
-    { src: potBlue.src, label: "Self-Development", pos: "", glow: potBlueGlow.src },
-    { src: potYellow.src, label: "Finance/Money", pos: "left-[50px]", glow: potYellowGlow.src },
-    { src: potGreen.src, label: "Health", pos: "left-[50px]", glow: potGreenGlow.src },
-    { src: potPurple.src, label: "Spiritual", pos: "", glow: potPurpleGlow.src },
+  {
+    src: "/icons/pot/potBlue.png",
+    label: "Self-Development",
+    pos: "",
+    glow: "/icons/potGlow/potBlueGlow.png",
+  },
+  {
+    src: "/icons/pot/potYellow.png",
+    label: "Finance/Money",
+    pos: "left-[50px]",
+    glow: "/icons/potGlow/potYellowGlow.png",
+  },
+  {
+    src: "/icons/pot/potGreen.png",
+    label: "Health",
+    pos: "left-[50px]",
+    glow: "/icons/potGlow/potGreenGlow.png",
+  },
+  {
+    src: "/icons/pot/potPurple.png",
+    label: "Spiritual",
+    pos: "",
+    glow: "/icons/potGlow/potPurpleGlow.png",
+  },
 ];
