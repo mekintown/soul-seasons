5169dbebb6f9137d20035d260213413fc5354a00
LittleNa1000 <naphanccnp@gmail.com>
2025-02-15 02:13:09 +0700

    feat: 5-2-4, 5-2-7

--- src/app/[locale]/(5)/5-2-4/page.tsx
+++ src/app/[locale]/(5)/5-2-4/page.tsx
@@ -1,35 +1,66 @@
 "use client"
-import DelayedFullScreenLink from "@/components/ui/DelayedFullScreenLink"
 import { motion } from "framer-motion"
 import { useTranslations } from "next-intl"
-import React from "react"
+import React, { useState } from "react"
 import Image from "next/image"
+import { useRouter } from "@/i18n/routing"
 
-const Scene6_3Page3: React.FC = () => {
+const Scene5_2Page4: React.FC = () => {
   const t = useTranslations("5-2-4")
+  const router = useRouter()
+  const [clickCount, setClickCount] = useState(0)
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null)
 
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center">
-      <div className="w-[200px] h-[200px] absolute bottom-[260px] left-[80px] rotate-90">
+    <div
+      className="flex h-screen w-full flex-col items-center justify-center"
+      onClick={() => {
+        setClickCount((prev) => prev + 1)
+        const currentClickCount = clickCount
+
+        // clear previous timeout and set new timeout
+        if (timeoutId) {
+          clearTimeout(timeoutId)
+        }
+        const newTimeoutId = setTimeout(() => {
+          if (currentClickCount === clickCount) {
+            // console.log("timeout", currentClickCount, clickCount)
+            router.push("/5-2-7")
+          }
+        }, 3000)
+        // console.log("currentClickCount", currentClickCount, timeoutId, newTimeoutId)
+        setTimeoutId(newTimeoutId)
+
+        // cutoff timer
+        setTimeout(() => {
+          // console.log("cutoff timer")
+          router.push("/5-2-7")
+        }, 15000)
+      }}>
+      <div
+        className={`w-[200px] h-[200px] absolute bottom-[260px] left-[80px] 
+          rotate-90
+          transform transition duration-700 ease-in  delay-500`}>
         <Image src="/background/fruit.webp" fill objectFit="contain" alt="christmas-tree" />
       </div>
-      <motion.div
-        className="absolute top-[73%]"
-        initial={{ opacity: 0, y: 0 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.5, duration: 1 }}>
-        <div className="flex h-auto w-full px-6">
-          <div className="items-cemter flex h-full w-full flex-col justify-center">
-            <p className="text-center text-lg text-white font-light whitespace-break-spaces drop-shadow-text leading-[24.68px]">
-              {t("lets")}
-            </p>
+      {clickCount >= 0 ? (
+        <motion.div
+          className="absolute top-[73%]"
+          initial={{ opacity: 0, y: 0 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 1, y: 0 }}
+          transition={{ delay: 7, duration: 1 }}>
+          <div className="flex h-auto w-full px-6">
+            <div className="items-cemter flex h-full w-full flex-col justify-center">
+              <p className="text-center text-lg text-white font-light whitespace-break-spaces drop-shadow-text leading-[24.68px]">
+                {t("haveYou")}
+              </p>
+            </div>
           </div>
-        </div>
-      </motion.div>
-      <DelayedFullScreenLink href="5-2-4" delay={1500} />
+        </motion.div>
+      ) : null}
     </div>
   )
 }
 
-export default Scene6_3Page3
+export default Scene5_2Page4
---
+++ src/app/[locale]/(5)/5-2-7/page.tsx
@@ -0,0 +1,25 @@
+"use client"
+import React, { useEffect, useState } from "react"
+import Image from "next/image"
+import DelayedFullScreenLink from "@/components/ui/DelayedFullScreenLink"
+
+const Scene5_2Page7: React.FC = () => {
+  const [animate, setAnimate] = useState(false)
+  useEffect(() => {
+    setAnimate(true)
+  })
+
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <div
+        className={`w-[200px] h-[200px] absolute bottom-[260px] left-[80px] ${
+          animate ? "rotate-0 translate-y-32 -translate-x-6" : "rotate-90"
+        }  transform transition duration-700 ease-in delay-500`}>
+        <Image src="/background/fruit.webp" fill objectFit="contain" alt="christmas-tree" />
+      </div>
+      <DelayedFullScreenLink href="6-1-1" delay={2000} />
+    </div>
+  )
+}
+
+export default Scene5_2Page7
