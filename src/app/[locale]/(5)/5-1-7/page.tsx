15712eb79df469e387b3bee9fdfaa6e6d68fab16
Jaiigh <jayjaycc2004@gmail.com>
2025-02-17 10:36:20 +0700

    5-1

--- src/app/[locale]/(5)/5-1-7/page.tsx
+++ src/app/[locale]/(5)/5-1-7/page.tsx
@@ -3,17 +3,21 @@ import DelayedFullScreenLink from "@/components/ui/DelayedFullScreenLink";
 import { motion } from "framer-motion";
 import { useTranslations } from "next-intl";
 import { getLocalStorageWithFallback } from "@/lib/localstorageUtils";
+import Image from "next/image";
 
-const Page5_1_1 = () => {
-  const t = useTranslations("5-1-1");
-  const importantPerson = getLocalStorageWithFallback(
-    "importantPerson",
-    t("importantPerson")
-  );
+const Page5_1_7 = () => {
+  const t = useTranslations("5-1-7");
   const name = getLocalStorageWithFallback("name", t("name"));
 
   return (
     <div className="flex h-screen flex-col text-white items-center justify-center relative">
+      <Image
+        src="/background/5-1-7.png"
+        alt="Tree"
+        layout="fill"
+        objectFit="cover"
+        className="absolute z-0"
+      />
       <motion.div
         style={{
           fontSize: "18px",
@@ -24,15 +28,16 @@ const Page5_1_1 = () => {
         animate={{ opacity: 1, z: 0, transition: { duration: 1, delay: 1 } }}
       >
         <p>
-          {importantPerson}: {name} {t("p1.s1")}
+          {name}: {t("p1.s1")}
         </p>
-        <p>{t("p1.s2")}</p>
+        <p> {t("p1.s2")}</p>
+        <p> {t("p1.s3")}</p>
       </motion.div>
 
       {/* Delay 2 seconds before navigating */}
-      <DelayedFullScreenLink href="5-1-2" delay={2000} />
+      <DelayedFullScreenLink href="5-1-8" delay={2000} />
     </div>
   );
 };
 
-export default Page5_1_1;
+export default Page5_1_7;
