1eeb12ca1a6ab2e083837b9b789dd984b2cfe2a2
Zeeneemee <106436879+Zeeneemee@users.noreply.github.com>
2025-02-18 22:48:09 +0700

    Add logic for season and chapter calculation (#26)
    * scene 0-0-1
    
    * scene 0-2
    
    add routing with handleclick
    
    * mobile display body
    
    * add font and dissolve annimation
    
    * add size and boxpadding props into messagebox
    
    * create shadow component
    
    * edit scene 0,0-1,0-2,0-4
    
    * add font kanit
    
    * scene 0-3-1,0-3-2
    
    * scene 0-4-1,0-4-3 & name input storing in local storage
    
    * edit routing and tailwind animation data type
    
    * Deleted comment add primary color and remove font cloud
    
    * Remove Trans
    
    * add scene 2, 2-1 and refactor with update i18n
    
    * edit the error in th.json  and en.json format
    
    * edit back eng.json
    
    * edit margin for 0-3-1
    
    * adjust  styling margins
    
    * remove unused file
    
    * adjust styling 0-3-1 and change routing for 0
    
    * Refactor
    
    * Refactor
    
    * Add tech debt
    
    * Add disabled btn
    
    * page 2-1 glow pots function and add localstorage
    
    * scene 2-1 add stressmotivation (localstorage) and glowing pots
    
    * Refactor tt-soul
    
    * add bar component state props
    
    * add font cloud add in 2-2 glow pots and refactor layout with i18n
    
    * changed font for scene0 and scene2
    
    * scene 3-1-1 to 3-1-6
    
    * glow pot sequence added
    
    * add in glowing pot sequence 2-2-2
    
    * Revert "glow pot sequence added"
    
    This reverts commit 36c707c7e8e237988bffaf503305d359cc26e3b9.
    
    * resolve git push bug
    
    * add in scene 2-2-1,2-2-2
    
    * added scene 3-1-1 - 3-1-5
    
    * tt-update merge main
    
    * fix conflict 1-4-6
    
    * adjust langtoggle
    
    * fix animated-image type
    
    * edit circle design
    
    * edit  conflict and add settimeout routes for 3-1-1 - 3-1-5
    
    * add in settimeout to router.push for scene 0 and 3-1
    
    * remake the pot circle adjustment logic
    
    * update i18n en.json and th.json
    
    * add translation to 2-2-2 pot page
    
    * obtain motivation, obstacles
    
    * logic final with no sleep
    
    * edit type logic
    
    * adjust comments on logic and import format
    
    * adjust i18n
    
    * refactor to enum and logic page
    
    * refactor logic
    
    * translation logic added
    
    * add enum logic to hook and pots
    
    ---------
    
    Co-authored-by: Maekin <74455990+mekintown@users.noreply.github.com>

---
+++ src/lib/motivations.ts
@@ -0,0 +1,13 @@
+import { Categories } from "./enum";
+export const motivationMap:  {[key: string]: Categories} = {
+    "Career/Work": Categories.Ambitious ,
+    "Family": Categories.HeartAndHome,
+    "Relationship/Friends": Categories.HeartAndHome,
+    "Sharing/Contributions": Categories.SoulfulImpact,
+    "Self-Development": Categories.SelfLove,
+    "Finance/Money": Categories.Ambitious ,
+    "Health": Categories.SelfLove,
+    "Spiritual": Categories.SoulfulImpact,
+
+};
+
