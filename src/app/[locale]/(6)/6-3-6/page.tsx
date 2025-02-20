e33a44dcd73c98db326326ad170bb9affb8acefa
Naphan Choatchuangnapha <83999105+LittleNa1000@users.noreply.github.com>
2025-02-20 10:30:21 +0700

    7-2 page without calculated results (#33)

--- src/app/[locale]/(6)/6-3-6/page.tsx
+++ src/app/[locale]/(6)/6-3-6/page.tsx
@@ -11,20 +11,21 @@ const Scene6_3Page6: React.FC = () => {
 
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center">
-      {/* <motion.div
-        className="absolute top-[61%]"
+      <motion.div
+        className="absolute bottom-[40%]"
         initial={{ opacity: 0, y: 0 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.5, duration: 1 }}>
+        transition={{ delay: 0.5, duration: 1 }}
+      >
         <div className="flex h-auto w-full px-6">
           <div className="items-cemter flex h-full w-full flex-col justify-center">
             <p className="text-center text-lg text-white font-light whitespace-break-spaces drop-shadow-text leading-[24.68px]">
-              {t("andAt")}
+              {t("couldYou")}
             </p>
           </div>
         </div>
-      </motion.div> */}
+      </motion.div>
       <motion.div
         className="absolute top-[61%] w-full"
         initial={{ opacity: 0, y: 0 }}
