e14d240e8861e841d993e47a2294e17f59f1281e
LittleNa1000 <naphanccnp@gmail.com>
2025-02-23 22:58:00 +0700

    feat: ambient sound setup

--- src/components/ui/sounds.tsx
+++ src/components/ui/sounds.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { scenePageMap, soundPageMap } from "@/lib/sounds";
+import { ambientPageMap, scenePageMap, soundPageMap } from "@/lib/sounds";
 import { createRef, useEffect, useState } from "react";
 import ReactHowler from "react-howler";
 import AudioPlayer from "./audio-player";
@@ -11,7 +11,11 @@ const Sounds = () => {
   const page = path.split("/")[1];
   const defaultBackingTrackVolume = 0.75;
   const defaultSoundEffectVolume = 0.3;
-  const scene = page.split("-")[0] as keyof typeof scenePageMap;
+  const defaultAmbientTrackVolume = 0.9;
+  const scene = page
+    .split("-")
+    .slice(0, 2)
+    .join("-") as keyof typeof scenePageMap;
 
   const [backingTrackSound, setBackingTrackSound] = useState<string>();
   const [backingTrackVolume, setBackingTrackVolume] = useState<number>(
@@ -19,13 +23,19 @@ const Sounds = () => {
   );
   const backingTrackRef = createRef<ReactHowler>();
   const soundEffectRef = createRef<ReactHowler>();
+  const ambientTrackRef = createRef<ReactHowler>();
   const [soundEffect, setSoundEffect] = useState<{
     sound: string;
     loop?: boolean;
     volume?: number;
+    replay?: boolean;
+  }>();
+  const [ambientTrack, setAmbientTrack] = useState<{
+    sound: string;
+    volume?: number;
   }>();
 
-  const fadeDuration = 1000;
+  const fadeDuration = 250;
 
   useEffect(() => {
     const nextSound = scenePageMap[scene];
@@ -41,13 +51,17 @@ const Sounds = () => {
   useEffect(() => {
     let nextSoundEffect: typeof soundEffect;
     let nextBackgroundVolume: number | undefined;
+    let nextAmbientTrack: typeof ambientTrack;
     if (page in soundPageMap) {
       nextSoundEffect =
         soundPageMap[page as keyof typeof soundPageMap]?.soundEffect;
       nextBackgroundVolume =
         soundPageMap[page as keyof typeof soundPageMap]?.backgroundVolume;
     }
-    if (nextSoundEffect !== soundEffect) {
+    if (page in ambientPageMap) {
+      nextAmbientTrack = ambientPageMap[page as keyof typeof ambientPageMap];
+    }
+    if (nextSoundEffect?.sound !== soundEffect?.sound) {
       if (soundEffectRef.current?.howler.playing()) {
         soundEffectRef.current?.howler.fade(
           soundEffectRef.current?.howler.volume() ?? defaultSoundEffectVolume,
@@ -60,6 +74,28 @@ const Sounds = () => {
       } else {
         setSoundEffect(nextSoundEffect);
       }
+    }
+    if (nextAmbientTrack?.sound !== ambientTrack?.sound) {
+      if (ambientTrackRef.current?.howler.playing()) {
+        ambientTrackRef.current?.howler.fade(
+          ambientTrackRef.current?.howler.volume() ?? defaultAmbientTrackVolume,
+          0,
+          fadeDuration
+        );
+        setTimeout(() => {
+          setAmbientTrack(nextAmbientTrack);
+        }, fadeDuration);
+      } else {
+        setAmbientTrack(nextAmbientTrack);
+      }
+    }
+
+    if (
+      nextSoundEffect?.sound === soundEffect?.sound &&
+      nextSoundEffect?.replay
+    ) {
+      soundEffectRef.current?.howler.stop();
+      soundEffectRef.current?.howler.play();
     }
 
     if (
@@ -96,6 +132,16 @@ const Sounds = () => {
           loop={soundEffect.loop}
           volume={soundEffect.volume ?? defaultSoundEffectVolume}
           ref={soundEffectRef}
+        />
+      )}
+      {ambientTrack && (
+        <AudioPlayer
+          key={ambientTrack.sound}
+          src={ambientTrack.sound}
+          loop
+          preload
+          volume={ambientTrack.volume ?? defaultAmbientTrackVolume}
+          ref={ambientTrackRef}
         />
       )}
     </>
--- src/lib/sounds.ts
+++ src/lib/sounds.ts
@@ -1,5 +1,7 @@
 export const scenePageMap = {
   "0": "/sound/track/0.mp3",
+  // "1-1": "/sound/bgm/B3 - Nostalgia.wav",
+  // "1-2": "/sound/bgm/B3 - Nostalgia.wav",
 } as const;
 
 export const soundPageMap: Record<
@@ -9,13 +11,75 @@ export const soundPageMap: Record<
       sound: string;
       loop?: boolean;
       volume?: number;
+      replay?: boolean;
     };
     backgroundVolume?: number;
   }
 > = {
-  "2-1": {
+  "1-3-1": {
     soundEffect: {
-      sound: "/sound/sound-effect/1.mp3",
+      sound: "/sound/effect/E8 - Autumn เสียงเดิน.wav",
+      volume: 1,
     },
+  },
+  "1-3-2": {
+    soundEffect: {
+      sound: "/sound/effect/E8 - Autumn เสียงเดิน.wav",
+      volume: 1,
+      replay: true,
+    },
+  },
+} as const;
+
+export const ambientPageMap: Record<
+  string,
+  {
+    sound: string;
+    volume?: number;
+  }
+> = {
+  "0-1": {
+    sound: "/sound/effect/E1 - 1 Intro Birds Wind.wav",
+  },
+  "0-2": {
+    sound: "/sound/effect/E1 - 1 Intro Birds Wind.wav",
+  },
+  "0-3-1": {
+    sound: "/sound/effect/E1 - 1 Intro Birds Wind.wav",
+  },
+  "0-3-2": {
+    sound: "/sound/effect/E1 - 1 Intro Birds Wind.wav",
+  },
+  "0-4-1": {
+    sound: "/sound/effect/E1 - 1 Intro Birds Wind.wav",
+  },
+  "0-4-2": {
+    sound: "/sound/effect/E1 - 1 Intro Birds Wind.wav",
+  },
+  "0-4-3": {
+    sound: "/sound/effect/E1 - 1 Intro Birds Wind.wav",
+  },
+  "0-5": {
+    sound: "/sound/effect/E1 - 1 Intro Birds Wind.wav",
+  },
+  "1-1-3": {
+    sound: "/sound/effect/E7 - ลม choice 2 (มีเสียงใบไม้เบาๆ).mp3",
+    volume: 1,
+  },
+  "1-2-1": {
+    sound: "/sound/effect/E6 - Wind.mp3",
+    volume: 1,
+  },
+  "1-2-2": {
+    sound: "/sound/effect/E6 - Wind.mp3",
+    volume: 1,
+  },
+  "1-2-4": {
+    sound: "/sound/effect/E6 - Wind.mp3",
+    volume: 1,
+  },
+  "1-2-5": {
+    sound: "/sound/effect/E6 - Wind.mp3",
+    volume: 1,
   },
 } as const;
